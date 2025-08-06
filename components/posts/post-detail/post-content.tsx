a9b8921bcd2d35747ef33b4bc24967fcf9fbdac9
Mkh <example@gmail.com>
2025-08-06 16:28:18 +0200

    adding-post-details-page

---
+++ components/posts/post-detail/post-content.tsx
@@ -0,0 +1,19 @@
+import classes from "./post-content.module.css";
+import PostHeader from "./post-header";
+
+const DUMMY_POST = {
+  slug: "getting-started-with-nextjs",
+  title: "Getting Started with NextJS",
+  image: "getting-started-nextjs.png",
+  date: "2022-02-10",
+  content: "# This is a first post",
+};
+
+export default function PostContent() {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  return (
+    <article className={classes.cotent}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+    </article>
+  );
+}
