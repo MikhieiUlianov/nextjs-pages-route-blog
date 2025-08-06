3c35fa64d5fb7568d3458ef89e32a256992e3d57
Mkh <example@gmail.com>
2025-08-06 16:32:04 +0200

    adding-markdown

--- components/posts/post-detail/post-content.tsx
+++ components/posts/post-detail/post-content.tsx
@@ -1,3 +1,5 @@
+import ReactMarkdown from "react-markdown";
+
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 
@@ -12,8 +14,9 @@ const DUMMY_POST = {
 export default function PostContent() {
   const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
   return (
-    <article className={classes.cotent}>
+    <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
     </article>
   );
 }
