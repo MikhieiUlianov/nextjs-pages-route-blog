c61a8395da55f424795d6138e08f379ba58ef320
Mkh <example@gmail.com>
2025-08-06 19:06:46 +0200

    reading-&-fetching-markdown-files

---
+++ lib/posts-util.tsx
@@ -0,0 +1,57 @@
+import fs from "fs";
+import path from "path";
+
+import matter from "gray-matter";
+
+const postsDirectory = path.join(process.cwd(), "posts");
+
+type PostMetaData = {
+  title: string;
+  image: string;
+  excerpt: string;
+  date: string;
+  isFeatured: boolean;
+};
+
+type PostFileType = PostMetaData & {
+  slug: string;
+  content: string;
+};
+function getPostData(fileName: string): PostFileType {
+  const filePath = path.join(postsDirectory, fileName);
+  //readFileSync will block for loop, until we parse that data
+  const fileContent = fs.readFileSync(filePath, "utf-8");
+  //returns an object with metadata (data) and a content (content).
+  //  Content : The actual content, the markup text as a string
+  const { data, content } = matter(fileContent);
+  //removes the file extension
+  const postSlug = fileName.replace(/\.md$/, "");
+
+  const postData = {
+    slug: postSlug,
+    ...(data as PostMetaData),
+    content,
+  };
+
+  return postData;
+}
+
+export function getAllPosts() {
+  //return an array of all file names
+  const postFiles = fs.readdirSync(postsDirectory);
+  const allPosts = postFiles.map((postFile) => {
+    return getPostData(postFile);
+  });
+
+  const sortedPosts = allPosts.sort((postA, postB) =>
+    postA.date > postB.date ? -1 : 1
+  );
+
+  return sortedPosts;
+}
+
+export function getFeaturedPosts() {
+  const allPosts = getAllPosts();
+  const featuredPosts = allPosts.filter((p) => p.isFeatured);
+  return featuredPosts;
+}
---
+++ posts/getting-started-with-nextjs.md
@@ -0,0 +1,13 @@
+---
+title: "Getting Started with NextJS"
+date: "2022-10-15"
+image: "getting-started-with-nextjs.png"
+excerpt: "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR."
+isFeatured: true
+---
+
+# This is a title
+
+<!-- this syntax means <p></p> -->
+
+This is some regular text with a [link](https://google.com)
