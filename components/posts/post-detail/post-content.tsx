43013d35a598a831d48b44597c71d2ac9ab7305e
Mkh <example@gmail.com>
2025-08-07 08:33:57 +0200

    rendering-code-snippets

--- components/posts/post-detail/post-content.tsx
+++ components/posts/post-detail/post-content.tsx
@@ -1,54 +1,44 @@
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 import { PostFileType } from "@/lib/posts-util";
 import Image from "next/image";
 
-const DUMMY_POST = {
-  slug: "getting-started-with-nextjs",
-  title: "Getting Started with NextJS",
-  image: "getting-started-nextjs.png",
-  date: "2022-02-10",
-  content: "# This is a first post",
-};
-
 export default function PostContent({ post }: { post: PostFileType }) {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   const customRenderers = {
-    /*  img(image){
-    return (
-      <Image
-        src={`/images/posts/getting-started-with-nextjs/${image.src}`}
-        alt={image.alt}
-        width={600}
-        height={300}
-      />
-    );
-  }, */
     p(paragraph: any) {
       const { node } = paragraph;
-
-      if (node.children[0]?.tagName === "img") {
-        const imageNode = node.children[0];
-
-        const src = imageNode.properties?.src;
-        const alt = imageNode.properties?.alt || "";
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
 
         return (
           <div className={classes.image}>
             <Image
-              src={`/images/posts/${post.slug}/${src}`}
-              alt={alt}
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.properties.alt}
               width={600}
               height={300}
             />
           </div>
         );
       }
-
       return <p>{paragraph.children}</p>;
+    },
+    code(code: any) {
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          children={children}
+          language={language}
+        />
+      );
     },
   };
 
